8335f0f2e2d0e498b26c1721ae8ee81cbdafdccb
michbaum <michbaum@student.ethz.ch>
2022-12-15 00:23:18 +0100

    Code review.

--- code/verifier.py
+++ code/verifier.py
@@ -202,6 +202,8 @@ def generate_alpha(in_lb: Tensor, in_ub: Tensor, strategy: str):
     elif strategy == "noisymin":
         min_scale = 0.75
         random_scales = min_scale + torch.rand(in_lb.shape) * (1 - min_scale)
+        # TODO: Does this really introduce symmetric noise?
+        # I think it works if the comparison is true, but not if it's false. Then we always get 0, no noise
         return (in_ub > -in_lb) * random_scales
     raise ValueError(f"{strategy} is an invalid alpha-generating strategy.")
 
@@ -379,17 +381,18 @@ def ensemble_poly(net_layers: Sequential, input_lb: Tensor, input_ub: Tensor, tr
     evolution_period = 5 if DEBUG else 10
     learning_rate = 10**0
     for epoch in range(max_iter):
+        # We iterate over our ensemble of 3 alpha strategies
         for i, (old_ub, alpha) in enumerate(zip(out_ubs, alphas)):
             # After the first epoch, we initialized the alphas for each strategy and do a Gradient Descent step
             if type(alpha) is not str:
                 # TODO?: According to documentation, gradients do get aggregated over time, do we need to null them here for each epoch? 
                 # Meaning alpha[k].grad.zero_() for k in alpha.keys()?
-                # Or is this solved by our design that we always detach the alphas and then build a new graph in the next forward pass? Would investigate.
+                # Or is this solved by our design that we always detach the alphas and then build a new graph in the next forward pass? Would investigate. -> Done
 
                 # We always optimize/differentiate w.r.t. the first unbeaten competitive class comparison
                 # The resulting gradient is stored in the alpha values, which are the leafes of the computational graph
                 # TODO?: Just for myself, check that the index here does not need to be updated, i.e. that the first unbeaten class is always at index 0 because
-                # we always remove the beaten classes. Should happen through changing the comparison layer and the execution of deep_poly, but just to be sure.
+                # we always remove the beaten classes. Should happen through changing the comparison layer and the execution of deep_poly, but just to be sure. -> Done
                 old_ub[0].backward()
                 alpha = {k: (ten - learning_rate * ten.grad).clamp(0, 1).detach().requires_grad_() for k, ten in alpha.items()}
             out_ub, out_alpha, _ = deep_poly(layers, alpha, input_lb, input_ub)
@@ -405,16 +408,19 @@ def ensemble_poly(net_layers: Sequential, input_lb: Tensor, input_ub: Tensor, tr
                 # We found an alpha that ruled out at least one category, so we update the comparison layer
                 dprint(f"{len(remaining_labels)} categories left to beat: {remaining_labels}.")
                 # TODO?: Dumb idea, but is there anything in the comp graph of layers that we lose when we do this override?
-                # Or is everything solved by the fact, that our next run of deep_poly will build a new comp graph?
+                # Or is everything solved by the fact, that our next run of deep_poly will build a new comp graph? -> yep
                 # TODO?: Also, maybe another stupid thought, but does this even help computation in any way? Since we'll optimize
                 # the alphas only regarding the first to-be-beaten category anyways, so the edges that fall away here don't contribute anyways?
-                # Of course it's very neat with the current implementation, but I don't know how big the overhead of rebuilding the network is.
+                # Of course it's very neat with the current implementation, but I don't know how big the overhead of rebuilding the network is. -> Overhead is negligible
                 layers = with_comparison_layer(net_layers, true_label, adversarial_labels=remaining_labels)
+            # Save the upper bound and the alpha values for the right strategy
             out_ubs[i], alphas[i] = out_ub, out_alpha
         dprint(f"out_ubs after epoch {epoch}: {[out_ub.detach().numpy() for out_ub in out_ubs]}")
         # Do a little bit of evolution, i.e. mutate the worst performing alpha after some epochs
         if epoch % evolution_period == 0 and epoch > 0:
+            # TODO: Is this a typo or am I stupid, the extra brackets after ReLU()?
             total_losses = Tensor([ReLU()(out_ub).sum() for out_ub in out_ubs])
+            # Worst performing strategy will be reinitialized with a noisy min strategy
             alphas[torch.argmax(total_losses)] = "noisymin"
     dprint(f"Failed to verify after {(datetime.now()-start_time).total_seconds()} seconds and {max_iter} epochs.")
     return False
